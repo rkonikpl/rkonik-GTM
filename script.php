71e31a55b27fe293781c76b1d799df4de53636d3
Rafał Kobyliński <rkonik@rkonik.com>
2022-08-22 01:12:37 +0200

    Setting the minimum parameters of the installation

--- script.php
+++ script.php
@@ -1,6 +1,27 @@
 <?php
+/**
+ * @package    Joomla.Administrator
+ *
+ * @author     rKonik Rafał Kobyliński
+ * @copyright  rKonik Rafał Kobyliński
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Installer\InstallerScript;
 
+class plgSystemrkonikgtmInstallerScript extends InstallerScript
+{
+	/**
+	 * Extension script constructor.
+	 *
+	 * @return  void
+	 */
+	public function __construct() {
+		$this->minimumJoomla = '4.0';
+		$this->minimumPhp = JOOMLA_MINIMUM_PHP;
+
+	}
+
+}
